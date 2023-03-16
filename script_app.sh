b03268e458eaaaad58033ad792a79b4185e44c84
Amine <a.lakhdari@outlook.com>
2023-03-16 21:48:14 +0100

    modification du script

--- script_app.sh
+++ script_app.sh
@@ -5,10 +5,10 @@ apt-get update
 apt-get install -y docker.io
 
 # Récupérer le Dockerfile
-curl https://raw.githubusercontent.com/username/repo/main/Dockerfile -o Dockerfile
+curl https://github.com/Amioss/pet-test.git/Dockerfile -o Dockerfile
 
 # Construire l'image Docker
-docker build -t nom-de-votre-image .
+docker build -t app_test .
 
 # Exécuter le conteneur Docker
 docker run -p 8080:8080 -p 3306:3306 -p 80:80 -d nom-de-votre-image
